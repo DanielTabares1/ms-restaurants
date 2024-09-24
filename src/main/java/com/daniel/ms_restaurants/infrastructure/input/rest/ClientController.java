7051b8838c32c4c9c70f2e1058cd2d8dac96c4a7
Daniel Tabares <daniel.tabares1@udea.edu.co>
2024-09-24 11:23:27 -0500

    fix: solved problem on list of restaurants

--- src/main/java/com/daniel/ms_restaurants/infrastructure/input/rest/ClientController.java
+++ src/main/java/com/daniel/ms_restaurants/infrastructure/input/rest/ClientController.java
@@ -1,12 +1,13 @@
 package com.daniel.ms_restaurants.infrastructure.input.rest;
 
+import com.daniel.ms_restaurants.application.dto.RestaurantResponse;
 import com.daniel.ms_restaurants.application.handler.IDishHandler;
+import com.daniel.ms_restaurants.application.handler.IRestaurantHandler;
 import com.daniel.ms_restaurants.domain.model.Dish;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +17,20 @@ import java.util.List;
 public class ClientController {
 
     private final IDishHandler dishHandler;
+    private final IRestaurantHandler restaurantHandler;
 
     @GetMapping("/dishes/{restaurantId}")
     public List<Dish> getAllDishesByRestaurantId(@PathVariable long restaurantId){
         return dishHandler.getAllDishesByRestaurantId(restaurantId);
+    }
+
+    @GetMapping("/client/restaurant")
+    public ResponseEntity<List<RestaurantResponse>> getAllRestaurants(
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize
+    ){
+        List<RestaurantResponse> restaurants = restaurantHandler.getAllRestaurants(pageNumber, pageSize);
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
     }
 
 }
